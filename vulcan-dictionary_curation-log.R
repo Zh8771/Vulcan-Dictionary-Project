c09ed121155220fb2a43d32e95956876a0130439
Zh8771 <gstrohl27@gmail.com>
2023-05-03 21:11:38 -0600

    realised I was working from the data on my local system and not from the ones I put on the git repo, so I reloaded those; created new tables with some of the extra information in the columns separated out into new columns. The next part will be tedious and time consuming, so let's commit these changes before we forget

--- vulcan-dictionary_curation-log.R
+++ vulcan-dictionary_curation-log.R
@@ -23,11 +23,11 @@ library(stringr)
 library(tidyverse)
 ## LOAD DATA ##
 # Vulcan-Federation Standard (English) Dictionary:
-V.FSE <- read.delim("~/590DataScience/vulcan-dictionary/V-FSE-raw.txt", stringsAsFactors=TRUE)
+V.FSE <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/V-FSE-raw.txt", stringsAsFactors=TRUE)
 # Federation Standard (English)-Vulcan Dictionary:
-FSE.V <- read.delim("~/590DataScience/vulcan-dictionary/FSE-V-raw.txt", stringsAsFactors=TRUE)
+FSE.V <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/FSE-V-raw.txt", stringsAsFactors=TRUE)
 # Vulcan-Klingon Dictionary:
-V.KLI <- read.delim("~/590DataScience/vulcan-dictionary/V-KLI-raw.txt", stringsAsFactors=TRUE)
+V.KLI <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/V-KLI-raw.txt", stringsAsFactors=TRUE)
 
 
 #### ---------- TASK 1: CLEAN UP RAW DATA ---------- ####
@@ -47,10 +47,10 @@ V.FSE <- data.frame(V.FSE) %>%
   separate(WORD.V,
            into = c("vulcan", "fse"),
            sep = "--")
-# I got an error message that row 627 (in $fse) is empty (NA). I'll go back and fix that in the data so it won't mess up the code.
+# DONE: I got an error message that row 627 (in $fse) is empty (NA). I'll go back and fix that in the data so it won't mess up the code.
 
 # WOOO I don't need this old code anymore HALLELUJAH (I'm keeping it though, because this code helps you see the steps)
-  # STEP 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
+  # Step 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
   # V.FSE1 <- data.frame(str_split_fixed(V.FSE$WORD.V, '--', 2))
   # Step 2: combine columns of new data frame with original
   # V.FSE2 <- cbind(V.FSE, V.FSE1)
@@ -70,90 +70,107 @@ write.table(V.FSE, file = "V-FSE-sep.txt", append = FALSE, sep = "\t", dec = "."
             row.names = TRUE, col.names = TRUE)
 
 #### REPEAT FOR FSE-VULCAN DICTIONARY ####
-# STEP 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
-FSE.V1 <- data.frame(str_split_fixed(FSE.V$WORD.FSE, '--', 2))
-# Step 2: combine columns of new data frame with original
-FSE.V2 <- cbind(FSE.V, FSE.V1)
-# Step 3: take out word.v and rename columns
-FSE.V3 <- subset(FSE.V2, select = -WORD.FSE)
-FSE.V <- FSE.V3 %>% 
-  rename("fse" = "X1",
-         "vulcan" = "X2")
-# Step 4: trim extra spaces
-FSE.V$vulcan <- trim(FSE.V$vulcan)
-FSE.V$fse <- trim(FSE.V$fse)
-# Remove excess tables
-rm(FSE.V1, FSE.V2, FSE.V3)
-# Step 4: Save resulting table in wd (working directory)
+# Hmm, I have old code, but let's try to simplify this a little, shall we?
+  # FSE.V <- data.frame(FSE.V) %>%
+  #   separate(WORD.FSE,
+  #            into = c("fse", "vulcan"),
+  #            sep = "--")
+# So this didn't work for some reason. (Error message: "Error in nchar(u, itype) : invalid multibyte string, element 1")
+# Let's use our old code for now...
+
+  # Step 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
+  FSE.V1 <- data.frame(str_split_fixed(FSE.V$WORD.FSE, '--', 2))
+  # Step 2: combine columns of new data frame with original
+  FSE.V2 <- cbind(FSE.V, FSE.V1)
+  # Step 3: take out word.v and rename columns
+  FSE.V3 <- subset(FSE.V2, select = -WORD.FSE)
+  FSE.V <- FSE.V3 %>% 
+    rename("fse" = "X1",
+           "vulcan" = "X2")
+  # Step 4: trim extra spaces
+  FSE.V$vulcan <- trim(FSE.V$vulcan)
+  FSE.V$fse <- trim(FSE.V$fse)
+  # Remove excess tables
+  rm(FSE.V1, FSE.V2, FSE.V3)
+
+# Save resulting table in wd (working directory)
 write.table(FSE.V, file = "FSE-V-sep.txt", append = FALSE, sep = "\t", dec = ".",
             row.names = TRUE, col.names = TRUE)
 
 #### REPEAT FOR KLINGON DICTIONARY ####
-# STEP 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
-V.KLI1 <- data.frame(str_split_fixed(V.KLI$WORD.V, '--', 2))
-# Step 2: combine columns of new data frame with original
-V.KLI2 <- cbind(V.KLI, V.KLI1)
-# Step 3: take out word.v and rename columns
-V.KLI3 <- subset(V.KLI2, select = -WORD.V)
-V.KLI <- V.KLI3 %>% 
-  rename("vulcan" = "X1",
-         "klingon" = "X2")
-# Step 4: trim extra spaces
-V.KLI$vulcan <- trim(V.KLI$vulcan)
-V.KLI$klingon <- trim(V.KLI$klingon)
-# Remove excess tables
-rm(V.KLI1, V.KLI2, V.KLI3)
-# Step 4: Save resulting table in wd (working directory)
+# Hmm, I have old code, but let's try to simplify this a little, shall we?
+V.KLI <- data.frame(V.KLI) %>%
+  separate(WORD.V,
+           into = c("vulcan", "klingon"),
+           sep = "--")
+
+# WOOO I don't need this old code anymore HALLELUJAH (I'm keeping it though, because this code helps you see the steps)
+  # Step 1:  Split word/meaning column into two separate columns, store as a data frame for ease of merging back into the full dataset
+  # V.KLI1 <- data.frame(str_split_fixed(V.KLI$WORD.V, '--', 2))
+  # Step 2: combine columns of new data frame with original
+  # V.KLI2 <- cbind(V.KLI, V.KLI1)
+  # Step 3: take out word.v and rename columns
+  # V.KLI3 <- subset(V.KLI2, select = -WORD.V)
+  # V.KLI <- V.KLI3 %>% 
+  #   rename("vulcan" = "X1",
+  #          "klingon" = "X2")
+  # Step 4: trim extra spaces
+  # V.KLI$vulcan <- trim(V.KLI$vulcan)
+  # V.KLI$klingon <- trim(V.KLI$klingon)
+  # Remove excess tables
+  # rm(V.KLI1, V.KLI2, V.KLI3)
+
+# Save resulting table in wd (working directory)
 write.table(V.KLI, file = "V-KLI-sep.txt", append = FALSE, sep = "\t", dec = ".",
             row.names = TRUE, col.names = TRUE)
 
-### REMOVE UNWANTED CHARACTERS & FORMATTING FROM V.FSE, FSE.V ###
-## NOTE: This section does not apply to the V.KLI data as it did not exhibit these issues.
-# Because the data was copied and pasted from a capture of a web page, there were html tags
-# and characters such as "<ca>" "�" and "\" leftover in the data after we separated it out.
-# Since these will make it harder for us to manipulate the data further and separate out all
-# of the useful bits of info, let's change or get rid of these. 
+#### This next section is also old, because I figured out some work-arounds for these issues! Keeping my notwes for posterity, tho! ####
+  ### REMOVE UNWANTED CHARACTERS & FORMATTING FROM V.FSE, FSE.V ###
+  ## NOTE: This section does not apply to the V.KLI data as it did not exhibit these issues.
+  # Because the data was copied and pasted from a capture of a web page, there were html tags
+  # and characters such as "<ca>" "�" and "\" leftover in the data after we separated it out.
+  # Since these will make it harder for us to manipulate the data further and separate out all
+  # of the useful bits of info, let's change or get rid of these. 
 
-## Step 1: Replace backslashes with forward slashes
-# I still want to keep the slashes in the data because they delimit some (albeit incomplete)
-# pronunciation data that I want to keep. However, I had to manipulate the data in excel to get
-# rid of the backslashes because they're a special operator in R and they would have interfered
-# with the code if I tried to replace them here, so the steps in this subsection were completed
-# in Excel with the 'find and replace' function.
+  ## Step 1: Replace backslashes with forward slashes
+  # I still want to keep the slashes in the data because they delimit some (albeit incomplete)
+  # pronunciation data that I want to keep. However, I had to manipulate the data in excel to get
+  # rid of the backslashes because they're a special operator in R and they would have interfered
+  # with the code if I tried to replace them here, so the steps in this subsection were completed
+  # in Excel with the 'find and replace' function.
 
-## Step 2: Get rid of html tags & extra formatting
-# I also used the 'find and replace' function to get rid of the "<ca>" tags and then used the
-# 'erase formatting' function to get rid of the "�" characters, as my professor and I think they
-#  were formatting instructions to make things italicised (although we could be wrong).
-# NOTE: The <ca> tags were replaced by "--" so that the information in the parentheses following
-# it could be extracted more easily by adapting previously used code.
+  ## Step 2: Get rid of html tags & extra formatting
+  # I also used the 'find and replace' function to get rid of the "<ca>" tags and then used the
+  # 'erase formatting' function to get rid of the "�" characters, as my professor and I think they
+  #  were formatting instructions to make things italicised (although we could be wrong).
+  # NOTE: The <ca> tags were replaced by "--" so that the information in the parentheses following
+  # it could be extracted more easily by adapting previously used code.
 
-# Now we'll read the new manipulated data back into R... (frmtd = formatted)
-V.FSE.frmtd <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/V-FSE-frmtd.txt", stringsAsFactors=TRUE)
-FSE.V.frmtd <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/FSE-V-frmtd.txt", stringsAsFactors=TRUE)
+  # Now we'll read the new manipulated data back into R... (frmtd = formatted)
+  # V.FSE.frmtd <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/V-FSE-frmtd.txt", stringsAsFactors=TRUE)
+  # FSE.V.frmtd <- read.delim("~/590DataScience/Vulcan-Dictionary-Project1/FSE-V-frmtd.txt", stringsAsFactors=TRUE)
 
-## Step 3: Hmm...those weren't there before...
-# The first thing I noticed about FSE.V.frmtd is that there are new <ca> tags around the parentheses
-# I'll try to get rid of them here in R...
-V.FSE.frmtd$vulcan <- gsub('[<ca>]','',V.FSE.frmtd$vulcan)
-FSE.V.frmtd$fse <- gsub('[<ca>]','',FSE.V.frmtd$fse)
+  ## Step 3: Hmm...those weren't there before...
+  # The first thing I noticed about FSE.V.frmtd is that there are new <ca> tags around the parentheses
+  # I'll try to get rid of them here in R...
+  # V.FSE.frmtd$vulcan <- gsub('[<ca>]','',V.FSE.frmtd$vulcan)
+  # FSE.V.frmtd$fse <- gsub('[<ca>]','',FSE.V.frmtd$fse)
 
-## Okay, great! So that code removes the need for this old code because the "�" isn't showing up! *phew*
-  # FSE.V.frmtd %>% 
-  #   mutate_all(funs(str_replace(., "<ca>", "--")))
-  # FSE.V.frmtd %>% 
-  # Oof running this creates new "�" thingies...Let's try a similar way to get rid of them...
-  #   mutate_all(funs(str_replace(., "�", "--")))
-  # Now let's trim off that pesky "�"!!
-  # FSE.V.frmtd$fse <- trim1(FSE.V.frmtd$fse)
+  ## Okay, great! So that code removes the need for this old code because the "�" isn't showing up! *phew*
+    # FSE.V.frmtd %>% 
+    #   mutate_all(funs(str_replace(., "<ca>", "--")))
+    # FSE.V.frmtd %>% 
+    # Oof running this creates new "�" thingies...Let's try a similar way to get rid of them...
+    #   mutate_all(funs(str_replace(., "�", "--")))
+    # Now let's trim off that pesky "�"!!
+    # FSE.V.frmtd$fse <- trim1(FSE.V.frmtd$fse)
 
-  # Let's try it on V.FSE...
-  # V.FSE.frmtd <- V.FSE.frmtd %>% 
-  #   mutate_all(funs(str_replace(., "<ca>", "--")))
-  # V.FSE.frmtd <- V.FSE.frmtd %>% 
-  #   mutate_all(funs(str_replace(., "�", "--")))
-  # V.FSE.frmtd$vulcan <- trim1(V.FSE.frmtd$vulcan)
-  # (F&çk yeah. Awesome. I'm so glad that worked!!)
+    # Let's try it on V.FSE...
+    # V.FSE.frmtd <- V.FSE.frmtd %>% 
+    #   mutate_all(funs(str_replace(., "<ca>", "--")))
+    # V.FSE.frmtd <- V.FSE.frmtd %>% 
+    #   mutate_all(funs(str_replace(., "�", "--")))
+    # V.FSE.frmtd$vulcan <- trim1(V.FSE.frmtd$vulcan)
 
 
 #### ---------- TASK 2: WIDEN, LENGTHEN DATA ---------- ####
@@ -165,7 +182,7 @@ FSE.V.frmtd$fse <- gsub('[<ca>]','',FSE.V.frmtd$fse)
 
 ### First, we'll separate out variety data and add the 'variety' column to V.FSE ###
 ## We have to do this one first because the variety data is at the end of the string in the cells that have them
-V.FSE1 <- data.frame(V.FSE.frmtd) %>%
+V.FSE1 <- data.frame(V.FSE) %>%
   separate(vulcan,
            into = c("vulcan", "variety"),
            sep = "\\(|\\)")
@@ -173,8 +190,6 @@ V.FSE1 <- data.frame(V.FSE.frmtd) %>%
 # since this is a Modern Golic dictionary, and where variety was not specified, it was understood to be MGV, so we want
 # our data to reflect this.
 V.FSE1$variety <- V.FSE1$variety %>% replace_na('MGV')
-# There's some part of speech data in our variety column that we'll have to separate out later, but for now, let's be happy
-# that all of this worked!
 
 ### Next, we'll Separate out (incomplete) pronunciation data from V.FSE ###
 # NOTE: The writers of the dictionary attempted to add pronunciations to some of the words;
@@ -190,7 +205,7 @@ V.FSE1$variety <- V.FSE1$variety %>% replace_na('MGV')
 V.FSE2 <- data.frame(V.FSE1) %>%
   separate(vulcan,
            into = c("vulcan", "pronunciation"),
-           sep = "\\/|\\/")
+           sep = "\\\\")
 
 # WOOO I don't need this old code anymore HALLELUJAH (I'm keeping it though, because this code helps you see the steps)
   # V.FSE2 <- data.frame(str_split_fixed(V.FSE1$vulcan, '\\/|\\/', 2))
@@ -211,23 +226,49 @@ V.FSE2 <- data.frame(V.FSE1) %>%
 
 ### Next, we'll separate out part of speech data and add the 'part of speech (pos)' column to V.FSE, FSE.V ###
 ## This has proven to be quite the challenge so let's start with the easy part first.
-
-# Step 1: get the data in parentheses into a separate column
-V.FSE2 <- data.frame(V.FSE1) %>%
+# Get the data in parentheses into a separate column (V.FSE)
+V.FSE <- data.frame(V.FSE2) %>%
   separate(fse,
       into = c("fse", "domain"),
       sep = "\\(|\\)")
-# What happened when we did this is that the domain data and the part of speech data were put into same column, so now we have to separate those out
+# Remove excess tables (I only have these excess tables in the first place so I can go back and change something without starting all the way over!)
+rm(V.FSE1,V.FSE2)
 
+# Save resulting table in wd (working directory)
+write.table(V.FSE, file = "V-FSE-var.txt", append = FALSE, sep = "\t", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+
+# Get the data in parentheses into a separate column (FSE.V)
+FSE.V <- data.frame(FSE.V) %>%
+  separate(fse,
+           into = c("fse", "domain"),
+           sep = "\\(|\\)")
+
+# Save resulting table in wd (working directory)
+write.table(FSE.V, file = "FSE-V-var.txt", append = FALSE, sep = "\t", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+
+# Now, since there are some parentheses in the V.KLI data, let's separate those out, too
+# Get the data in parentheses into a separate column (FSE.V)
+V.KLI <- data.frame(V.KLI) %>%
+  separate(vulcan,
+           into = c("vulcan", "domain"),
+           sep = "\\(|\\)")
+# Interesting, there's an 'adv.' in there!
+# We'll have to separate those out, but that's a later-us problem.
+
+# Save resulting table in wd (working directory)
+write.table(V.KLI, file = "V-KLI-var.txt", append = FALSE, sep = "\t", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+
+### Now, we need to separate out the values in the new columns ###
+## What happened when we ran the part of the code immediately above is that the domain data and the part of speech data were put into same column, so now we have to separate those out
 ## Now what I'm trying to do is make a separate column with only the part of speech data in it (of which only adjectives,
 ## nouns, prepositions, and verbs are marked). Since the column that gets created when I run the separation consists
 ## of more than just the part of speech data, I need to separate rows that have one of the parts of speech into one
 ## column, and the rest of the rows into a separate column.
 
-# Step 2: Separate out the domain and extra material from the pos column so that I can see what the levels are
-# An example of code to add a column based on if_else
-# starwars %>%
-#   mutate(category = if_else(height < 100, "short", "tall"), .keep = "used")
+## Step 1: Separate out the domain and extra material from the pos column, V.FSE
 
 # My try would be...
 # V.FSE2 %>%
@@ -242,5 +283,5 @@ V.FSE2 <- data.frame(V.FSE1) %>%
 ### Next, we'll separate out semantic domain data and add the 'domain' column to V.FSE, FSE.V ###
 
 
-#### TASK 4: (if there's time) Add concept data from concepticon, new Vulc-concepticon ####
+#### TASK 3: (if there's time) Add concept data from concepticon, new Vulc-concepticon ####
 # Vulc-concepticon = new table I will create with vulcan-specific and trek-specific concepts
